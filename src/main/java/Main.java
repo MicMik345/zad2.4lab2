80bde24a7e0d7c2b5a0b1a5edc24f8ffc9dd8f54
MicMik345 <177254@stud.prz.edu.pl>
2025-03-26 14:14:56 +0000

    zad.5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,28 +3,16 @@ import java.util.Random;
 
 public class Main {
   public static void main(String[] args) {
-    
 
-    Random random = new Random();
 
-    
-    
-    int rand1 = random.nextInt(50);
-    int rand2 = random.nextInt(50);
-    int rand3 = random.nextInt(50);
-    int rand4 = random.nextInt(50);
-    int rand5 = random.nextInt(50);
-   
-    
-
-    int suma = (rand1*rand1) + (rand2*rand2) + (rand3*rand3)+(rand4*rand4)+(rand5*rand5);
-    
- 
-    System.out.println("Wylosowane liczby to: " + rand1 + ", " + rand2 + ", " + rand3 + ", " + rand4 + ", " + rand5);
-    System.out.println("Wynik sumy kwadratów to: " + suma);
-  
-  
-  ///dsdasd
+    int suma = 0;
+  for(int i = 0; i < 100 ; i++){
+    if(i % 2 != 0){
+      suma = suma + i;
+    }
+    System.out.println(suma);
+  }
+    System.out.println("Suma liczb nieparzystych wynosi: " + suma);
   
   
   
