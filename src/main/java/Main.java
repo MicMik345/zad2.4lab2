7cf39e567ab15ef201bd17e3306612b29d3d3c21
MicMik345 <177254@stud.prz.edu.pl>
2025-03-26 14:23:50 +0000

    zad2.5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,26 @@ import java.util.Random;
 public class Main {
   public static void main(String[] args) {
 
+    System.out.println("Witaj w kalkulatorze");
+    System.out.println("Podaj 3 liczby");
 
-    int suma = 0;
-  for(int i = 0; i < 100 ; i++){
-    if(i % 2 != 0){
-      suma = suma + i;
+    
+    
+    System.out.println("Wybierz opcje: 1=obliczanie 2=trojkatprotokatny 3=obliczanie2");
+
+     int numer = 0;
+    switch(numer) {
+      case 1:
+        // code block
+        break;
+      case 2:
+        // code block
+        break;
+      case 3:
+        
+      default:
+        // code block
     }
-    System.out.println(suma);
-  }
-    System.out.println("Suma liczb nieparzystych wynosi: " + suma);
   
   
   
